a6d3c0dd195defe2826b6ba23347599637c65d5c
unknown <amr.magdy.ahmed92@gmail.com>
2024-06-10 10:31:50 +0300

    Added the initial app and server files

---
+++ server/app.js
@@ -0,0 +1,69 @@
+// Base Module
+import express from "express"
+
+// Security Modules
+import cors from "cors"
+import helmet from "helmet"
+import csp from "helmet-csp"
+import expressRateLimiter from "express-rate-limit"
+
+// Environment configuration
+import dotenv from "dotenv"
+
+// Request handlers
+import bodyParser from "body-parser"
+import compress from "compression"
+
+// Routers
+
+// app configuration
+dotenv.config()
+const configuredBodyParserJSON = bodyParser.json()
+const configuredBodyParserURLEncoding = bodyParser.urlencoded({ extended: true })   
+const configuredCompress = compress()
+
+// Security Configurations
+const configuredHelmet = helmet({ crossOriginResourcePolicy: false })
+const configuredCSP = csp({
+    directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self' 'unsafe-inline' https://cdn.jsdelivr.net"],
+        styleSrc: ["'self' 'unsafe-inline' https://cdn.jsdelivr.net"],
+        imgSrc: ["'self' 'unsafe-inline' https://encrypted-tbn0.gstatic.com/ https://encrypted-tbn1.gstatic.com/ https://encrypted-tbn2.gstatic.com/ https://encrypted-tbn3.gstatic.com/ https://apod.nasa.gov/"]
+    }
+})
+const configuredCors = cors({
+    origin: process.env.CORS_ORIGIN,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: process.env.CORS_CREDENTIALS
+})
+const configuredRateLimiter = expressRateLimiter({
+    window: process.env.RATE_WINDOW,
+    max: process.env.RATE_MAX,
+    message: process.env.RATE_MESSAGE,
+    Headers: true,
+    keyGenerator: (req, res) => {
+        return req.ip
+    }
+})
+
+// App initialization
+const app = express()
+app.use(configuredBodyParserJSON)
+app.use(configuredBodyParserURLEncoding)
+app.use(configuredCompress)
+app.use(configuredHelmet)
+app.use(configuredCSP)
+app.use(configuredCors)
+app.use(configuredRateLimiter)
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.send("index.html")
+})
+
+// Routes
+
+// Export
+export default app
