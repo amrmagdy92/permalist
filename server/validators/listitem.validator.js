a85b797284f345d9509d4cec36dd62c093f99dc8
unknown <amr.magdy.ahmed92@gmail.com>
2024-06-14 14:42:14 +0300

    refactored validations for better separation

--- server/validators/listitem.validator.js
+++ server/validators/listitem.validator.js
@@ -1,11 +1,19 @@
-const validateListItem = (listItemData) => {
+const validateStatusFilter = (filter) => {
     let errors = {}
     const validItemStatus = ["to_do", "in_progress", "done"]
+    !validItemStatus.includes(filter)? errors.item_status = "Invalid list item status": null
+    return errors
+}
+
+const validateListItem = (listItemData) => {
+    let errors = {}
+    let filterErrors = validateStatusFilter(listItemData.status)
+    Object.keys(filterErrors).length > 0? errors.item_status = filterErrors.item_status : null
     listItemData.item_text == "" || typeof listItemData.item_text === "undefined"? errors.item_text = "Item requires a description text" : null
-    !validItemStatus.includes(listItemData.status)? errors.item_status = "Item requires status text": null
     return errors
 }
 
 export {
-    validateListItem
+    validateListItem,
+    validateStatusFilter
 }
